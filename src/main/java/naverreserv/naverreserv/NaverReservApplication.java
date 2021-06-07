f43c0e2253495bebe32983b0e8a665fe17ddbd98
Slowth-KIM <rt.slowth@gmail.com>
2021-06-07 19:19:28 +0900

    프로젝트 초기 생성

---
+++ src/main/java/naverreserv/naverreserv/NaverReservApplication.java
@@ -0,0 +1,13 @@
+package naverreserv.naverreserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverReservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverReservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/naverreserv/naverreserv/NaverReservApplicationTests.java
@@ -0,0 +1,13 @@
+package naverreserv.naverreserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverReservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
